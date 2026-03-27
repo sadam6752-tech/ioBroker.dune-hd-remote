1bc8522a1416b391cd66a60523f2f25e80d12522
Alex1808 <sadam6752@gmail.com>
2026-03-27 18:08:48 +0100

    v1.1.3: Use standard workflow and testing scripts as provided by create-adapter

--- test/package/packageFiles.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { tests } = require('@iobroker/testing');
-const path = require('node:path');
-
-// Run package tests — point to adapter root
-tests.packageFiles(path.join(__dirname, '../..'), { /* options */ });
